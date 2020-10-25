de9c8e3aa23135b7a95012d379ac97c9369553b5
Ankush Dhawan <>
2020-10-25 15:40:45 +0530

    working on api integration

---
+++ IosPoc/IosPoc/ServiceClient/CountryDetailApiClient.swift
@@ -0,0 +1,27 @@
+//
+//  CountryDetailApiClient.swift
+//  IosPoc
+//
+//  Created by Ankush Dhawan on 10/25/20.
+//  Copyright © 2020 Ankush Dhawan. All rights reserved.
+//
+
+import Foundation
+
+class CountryDetailApiClient  {
+   var client:APIClient
+    init(client: APIClient) {
+        self.client = client
+    }
+    //If user pass nothing then default api service class object is pass otherwise user can mock class object to get data from json file
+    convenience  init() {
+        self.init(client: APIService())
+    }
+    
+    func fetchCountryData(resource : JCAPIResource,completion: @escaping (APIResponse<CountryModel, APIError>) -> Void) {
+        client.fetch(with: resource , decode: { json -> CountryModel? in
+            guard let user = json as? CountryModel else { return  nil }
+            return user
+        }, completion: completion)
+    }
+}
---
+++ IosPoc/Pods/Target Support Files/Pods-IosPoc/Pods-IosPoc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IosPoc : NSObject
+@end
+@implementation PodsDummy_Pods_IosPoc
+@end
