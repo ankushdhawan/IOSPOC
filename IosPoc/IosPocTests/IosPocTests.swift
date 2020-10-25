4fff853f3fb3aff69c25148838c241aded7019be
Ankush Dhawan <>
2020-10-25 14:46:02 +0530

    initail project added

---
+++ IosPoc/IosPocTests/IosPocTests.swift
@@ -0,0 +1,34 @@
+//
+//  IosPocTests.swift
+//  IosPocTests
+//
+//  Created by Ankush Dhawan on 10/25/20.
+//  Copyright © 2020 Ankush Dhawan. All rights reserved.
+//
+
+import XCTest
+@testable import IosPoc
+
+class IosPocTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
